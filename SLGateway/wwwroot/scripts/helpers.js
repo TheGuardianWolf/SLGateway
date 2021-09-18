a180568b80f38601ca95f156e1c7423b68180aec
TheGuardianWolf <nano@pixelcollider.net>
2021-09-19 07:38:39 +1200

    Reveal new api key

---
+++ SLGateway/wwwroot/scripts/helpers.js
@@ -0,0 +1,7 @@
+var selectElementText = function (elementId) {
+    var el = document.getElementById(elementId);
+    if (el.select) {
+        el.select();
+    }
+};
+//# sourceMappingURL=helpers.js.map
---
+++ SLGateway/wwwroot/scripts/helpers.ts
@@ -0,0 +1,7 @@
+﻿const selectElementText = (elementId: string) => {
+    const el = document.getElementById(elementId) as HTMLInputElement;
+
+    if (el.select) {
+        el.select();
+    }
+}
